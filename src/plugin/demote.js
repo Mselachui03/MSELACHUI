456a9a098a391faf083d0a19a08fb1edfa5a1845
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-03 00:41:28 +0300

    Update demote.js

--- src/plugin/demote.js
+++ src/plugin/demote.js
@@ -10,15 +10,15 @@ const demote = async (m, gss) => {
     if (!validCommands.includes(cmd)) return;
 
 
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🐯 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await gss.decodeJid(gss.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🐯 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!senderAdmin) return m.reply("*🐯 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
 
     if (!m.mentionedJid) m.mentionedJid = [];
 
@@ -31,7 +31,7 @@ const demote = async (m, gss) => {
       : [];
 
     if (users.length === 0) {
-      return m.reply("*📛 PLEASE MENTION OR QUOTE A USER TO DEMOTE*");
+      return m.reply("*🐯 PLEASE MENTION OR QUOTE A USER TO DEMOTE*");
     }
 
     const validUsers = users.filter(Boolean);
