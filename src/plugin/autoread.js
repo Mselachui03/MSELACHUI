1999cfecbc36216d1086614c5f0ef5621ae6ecbe
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-03 00:32:13 +0300

    Update autoread.js

--- src/plugin/autoread.js
+++ src/plugin/autoread.js
@@ -9,7 +9,7 @@ const autoreadCommand = async (m, Matrix) => {
   const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
 
   if (cmd === 'autoread') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*🐯 THIS IS AN OWNER COMMAND*");
     let responseMessage;
 
     if (text === 'on') {
