ac83a2664e5eaaba5b367224008bae1885576232
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-03 00:34:12 +0300

    Update autotyping.js

--- src/plugin/autotyping.js
+++ src/plugin/autotyping.js
@@ -9,7 +9,7 @@ const autotypingCommand = async (m, Matrix) => {
   const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
 
   if (cmd === 'autotyping') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*🐯 THIS IS AN OWNER COMMAND*");
     let responseMessage;
 
     if (text === 'on') {
